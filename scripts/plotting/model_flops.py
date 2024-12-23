3afa9b1f6fbcfad37677c4c494e063db2242b2aa
apoorvkh <mail@apoorvkh.com>
2024-12-23 00:53:52 -0500

    switching from pandas to great_tables for latex table generation

--- scripts/plotting/model_flops.py
+++ scripts/plotting/model_flops.py
@@ -1,33 +1,31 @@
 import marimo
 
-__generated_with = "0.8.18"
+__generated_with = "0.10.6"
 app = marimo.App(width="medium")
 
 
 @app.cell
-def __():
+def _():
     from experiments.count_flops_sweep import CountFlopsSweep
-
     return (CountFlopsSweep,)
 
 
 @app.cell
-def __(CountFlopsSweep):
+def _(CountFlopsSweep):
     results = CountFlopsSweep(search_space="experiments/sweep_configs/count_flops/all.json").results()
     return (results,)
 
 
 @app.cell
-def __():
+def _():
     import math
 
     import polars as pl
-
     return math, pl
 
 
 @app.cell
-def __(math, pl, results):
+def _(pl, results):
     _results = results.select(["model", "training_flops"])
 
     _results = _results.with_columns(
@@ -46,20 +44,22 @@ def __(math, pl, results):
         )
     )
 
-    def _sci_not(x):
-        exponent = int(math.log10(abs(x)))
-        coefficient = x / 10**exponent
-        return f"${coefficient:.1f} \\times 10^{{{exponent}}}$"
-
-    _table = _results.to_pandas().to_latex(
-        header=["Model", "Training FLOPs"],
-        index=False,
-        column_format="rc",
-        float_format=_sci_not,
-        escape=True,
+    _latex_table = (
+        _results.style.cols_label({"model": "Model", "training_flops": "Training FLOPs"})
+        .cols_align(align="right", columns=["model"])
+        .cols_align(align="center", columns=["training_flops"])
+        .fmt_scientific(columns="training_flops", decimals=1, exp_style="x10n")
+        .as_latex()
     )
 
-    _table
+    _latex_table = _latex_table[
+        _latex_table.find("\\begin{tabular*}") : _latex_table.find("\n\\end{table}")
+    ]
+    _latex_table = _latex_table.replace(
+        "\\begin{tabular*}{\\linewidth}{@{\\extracolsep{\\fill}}", "\\begin{tabular}{"
+    ).replace("\\end{tabular*}", "\\end{tabular}")
+
+    _latex_table
     return
 
 
